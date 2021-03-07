16e06a41f6f4ec00cdae58705e2595543ce12d12
jakewoodtracy <58447737+jakewoodtracy@users.noreply.github.com>
2021-03-07 10:56:39 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,428 @@
+var r1 = {
+    "directions": [
+        "Place eggplant on paper towels; cover with coarse salt. Let sit until moisture is drawn out, about 1 hour. Rinse with cold water; dry thoroughly with paper towels.",
+        "Preheat oven to 350 degrees F (175 degrees C). Grease a 9-inch round cake pan with cooking spray.",
+        "Heat olive oil in a skillet over medium heat. Add eggplant; cook and stir until eggplant is tender and lightly brown, about 5 minutes. Drain on paper towels.",
+        "Cover the bottom of the prepared baking pan with half of the eggplant; layer on ham, mozzarella cheese, and basil. Top with remaining eggplant.",
+        "Whisk eggs, Parmesan cheese, salt, and pepper together in a bowl; pour over the top eggplant layer.",
+        "Bake in the preheated oven until eggs are set and top is golden, about 30 minutes. Cool slightly, about 10 minutes. Flip the pan onto a plate to remove the quiche."
+    ],
+    "ingredients": [
+        "4 eggplants, peeled and cut into 1/4-inch-thick slices",
+        "coarse salt",
+        "cooking spray",
+        "2 tablespoons olive oil",
+        "10 1/2 ounces diced cooked ham",
+        "2 1/2 cups shredded mozzarella cheese",
+        "1/4 cup chopped fresh basil",
+        "4 eggs",
+        "1/2 cup freshly grated Parmesan cheese",
+        "salt and freshly ground black pepper to taste"
+    ],
+    "language": "en-US",
+    "source": "allrecipes.com",
+    "tags": [],
+    "title": "Aubergine Quiche (Eggplant Quiche)",
+    "url": "http://allrecipes.com/recipe/255023/aubergine-quiche-eggplant-quiche/"
+}
+
+var r2 = {
+    "directions": [
+        "Pur\u00e9e 1 very ripe peeled banana (preferably frozen), 2 pitted Medjool dates, 1 cup frozen mango chunks, 3/4 cup coconut water, 1/2 cup frozen blackberries or blueberries, 1/4 cup plain yogurt, and 1 tablespoon ground flaxseeds in a blender until smooth."
+    ],
+    "ingredients": [
+        "1 very ripe peeled banana (preferably frozen)",
+        "2 pitted Medjool dates",
+        "1 cup frozen mango chunks",
+        "3/4 cup coconut water",
+        "1/2 cup frozen blackberries or blueberries",
+        "1/4 cup plain yogurt",
+        "1 tablespoon ground flaxseeds"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Smoothie",
+        "Non-Alcoholic",
+        "Breakfast",
+        "Brunch",
+        "Vegetarian",
+        "Kid-Friendly",
+        "Yogurt",
+        "High Fiber",
+        "Blackberry",
+        "Blueberry",
+        "Date",
+        "Banana",
+        "Mango",
+        "Healthy",
+        "Low Cholesterol",
+        "Drink",
+        "Quick and Healthy",
+        "Pescatarian",
+        "Wheat/Gluten-Free",
+        "Peanut Free",
+        "Tree Nut Free",
+        "Soy Free",
+        "No Sugar Added",
+        "Kosher"
+    ],
+    "title": "Mango, Berry & Banana Smoothie",
+    "url": "http://www.epicurious.com/recipes/food/views/mango-berry-banana-smoothie-51143830"
+}
+
+var r3 = {
+    "directions": [
+        "Pat chicken dry and season with 3/4 teaspoon salt and 1/2 teaspoon pepper. Heat butter in a wide heavy 7- to 8-quart pot over moderately high heat until foam subsides, then brown chicken in 2 batches, skin side down first, turning occasionally, about 10 minutes. Transfer chicken as browned to a plate.",
+        "Add onion to pot along with oregano and remaining teaspoon each salt and pepper and saut\u00e9, stirring, until light golden, about 5 minutes. Peel and coarsely grate russet potatoes and add to pot with chicken, broth, and water. Simmer, covered, stirring occasionally, until chicken is cooked through, about 25 minutes. Transfer chicken with tongs to a cutting board to cool. Drain cubed yellow potatoes and add to pot.",
+        "Simmer, covered, stirring occasionally, until cubed potatoes are almost tender, about 10 minutes. Add corn and simmer, covered, until tender, 5 to 10 minutes more. While corn is cooking, remove skin and bones from chicken and coarsely shred meat. Add meat to pot and heat through.",
+        "Serve stew with accompaniments in separate bowls."
+    ],
+    "ingredients": [
+        "1 (3 1/2- to 4-lb) chicken, cut into 8 serving pieces",
+        "1 3/4 teaspoons salt",
+        "1 1/2 teaspoons black pepper",
+        "3 tablespoons unsalted butter",
+        "1 large white onion, finely chopped",
+        "2 teaspoons dried oregano, crumbled",
+        "1 1/2 lb russet (baking) potatoes",
+        "6 cups chicken broth",
+        "1 cup water",
+        "2 lb yellow-fleshed potatoes such as Yukon Gold, peeled, cut into 1/2-inch cubes, and covered with water in a bowl",
+        "3 ears corn, cut crosswise into 1-inch pieces",
+        "1/2 cup chopped fresh cilantro leaves",
+        "1 cup heavy cream",
+        "3 tablespoons drained capers",
+        "3 California avocados, quartered, pitted, peeled, and cut into 1/2-inch cubes"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Soup/Stew",
+        "Chicken",
+        "Potato",
+        "Poultry",
+        "Stew",
+        "Avocado",
+        "Corn",
+        "Fall",
+        "Gourmet"
+    ],
+    "title": "Colombian Chicken, Corn, and Potato Stew",
+    "url": "http://www.epicurious.com/recipes/food/views/colombian-chicken-corn-and-potato-stew-105721"
+}
+
+var r4 = {
+    "directions": [
+        "1. Deep-fry the plantain and potato separately in the canola oil until golden brown.",
+        "2. In a large bowl, whisk together the eggs and heavy cream; add the paprika, salt, and pepper.",
+        "3. Place the olive oil and butter in a 10-inch saut\u00e9 pan with an ovenproof handle over medium-high heat. When the butter has melted, add the onion and saut\u00e9 until brown.",
+        "4. Add the plantain and potato to the egg mixture, then pour it into the pan. Cook for 5 minutes, scraping the pan regularly so the bottom sets.",
+        "5. Place under a broiler until golden brown, 7 to 10 minutes. Serve with a simple mesclun salad (optional)."
+    ],
+    "ingredients": [
+        "1 large ripe plantain, cut into 1/2-inch cubes",
+        "1 medium baking potato, peeled and cut into 1/2-inch cubes",
+        "Canola oil",
+        "8 large eggs",
+        "1/2 cup heavy cream",
+        "1 teaspoon sweet paprika",
+        "Salt and pepper to taste",
+        "3 tablespoons olive oil",
+        "2 tablespoons butter",
+        "1 large yellow onion, diced"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Egg",
+        "Onion",
+        "Potato",
+        "Broil",
+        "Saut\u00e9",
+        "Vegetarian",
+        "Kid-Friendly",
+        "Cookie"
+    ],
+    "title": "Classic Havana Frittata",
+    "url": "http://www.epicurious.com/recipes/food/views/classic-havana-frittata-239144"
+}
+
+var r5 = {
+    "directions": [
+        "Heat the olive oil and butter in a skillet over medium heat. Stir in the bell pepper and onion; cook and stir until the onion has softened and turned translucent, about 5 minutes. Stir in the garlic, and cook until fragrant, about 1 minute more. Add the shrimp and cook until opaque, about 2 minutes on each side. Stir in the tomato sauce, parsley, and red pepper flakes, and cook until tomato sauce is warmed through, about 5 minutes. Season with salt and pepper."
+    ],
+    "ingredients": [
+        "1 tablespoon olive oil",
+        "1/4 cup butter",
+        "1/2 green bell pepper, sliced",
+        "1 large onion, sliced",
+        "10 cloves garlic, pressed",
+        "1 pound fresh large shrimp, shelled and deveined",
+        "1/2 cup tomato sauce",
+        "1 tablespoon dried parsley",
+        "1/4 teaspoon crushed red pepper flakes (optional)",
+        "salt and ground black pepper to taste"
+    ],
+    "language": "en-US",
+    "source": "allrecipes.com",
+    "tags": [],
+    "title": "Camarones al Ajillo (Garlic Shrimp)",
+    "url": "http://allrecipes.com/recipe/217984/camarones-al-ajillo-garlic-shrimp/"
+}
+var r6 = {
+    "directions": [
+        "Place apricots and plums in small heatproof bowl. Add enough boiling water to cover. Let stand 15 minutes to soften. Drain and quarter all fruit.",
+        "Meanwhile, mix 2/3 cup broth with soy sauce, mango chutney, tamarind concentrate, and sugar in small bowl. Whisk cumin, curry powder, garlic powder, and cayenne pepper in another small bowl. DO AHEAD: Fruit, broth mixture, and spice blend can be made 2 hours ahead. Let stand separately at room temperature.",
+        "Heat vegetable oil in heavy large skillet over medium-high heat. Add onion and saut until translucent, about 2 minutes. Add garlic and ginger and saut 30 seconds. Add apricots, plums, and carrot; saut until onion begins to brown, about 1 minute. Add shrimp and spice blend, stirring to coat. Cook until shrimp are pink on both sides but still uncooked in center, about 2 minutes. Add broth mixture; cover and cook until shrimp are just opaque in center, about 1 minute, adding more broth by tablespoonfuls if sauce is too thick. Transfer to serving bowl. Sprinkle with green onion and serve.",
+        "*Tamarind concentrate is a dark, seedless paste (sometimes labeled tamarind paste) with a tart-sweet flavor; available at Middle Eastern and Indian markets and at some Asian markets."
+    ],
+    "ingredients": [
+        "12 dried apricot halves",
+        "12 pitted dried plums (prunes)",
+        "2/3 cup (or more) low-salt chicken broth",
+        "2 tablespoons soy sauce",
+        "1 tablespoon mango chutney, large pieces chopped",
+        "2 teaspoons tamarind concentrate*",
+        "1 teaspoon sugar",
+        "1 1/4 teaspoons ground cumin",
+        "1 1/4 teaspoons curry powder",
+        "1 1/4 teaspoons garlic powder",
+        "1/2 teaspoon cayenne pepper",
+        "3 tablespoons vegetable oil",
+        "1/2 cup thinly sliced white onion",
+        "2 garlic cloves, minced",
+        "1 teaspoon chopped peeled fresh ginger",
+        "1/2 cup matchstick-size strips peeled carrot",
+        "16 uncooked large shrimp, peeled, deveined",
+        "1 green onion, chopped"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Ginger",
+        "Onion",
+        "Saut\u00e9",
+        "Currant",
+        "Dried Fruit",
+        "Prune",
+        "Apricot",
+        "Shrimp",
+        "Carrot",
+        "Winter",
+        "Tamarind",
+        "Soy Sauce"
+    ],
+    "title": "Ginger-Apricot Shrimp",
+    "url": "http://www.epicurious.com/recipes/food/views/ginger-apricot-shrimp-239844"
+}
+
+var r7 = {
+    "directions": [
+        "1. Combine avocado, basil, pine nuts, and garlic in a food processor or blender. With machine running, slowly stream in the lemon juice, olive oil, and water as needed to achieve a smooth pesto. Puree until completely blended and creamy.",
+        "2. Season with salt and pepper to taste.",
+        "3. Use immediately or keep, refrigerated, for up to four hours."
+    ],
+    "ingredients": [
+        "1 ripe avocado, peeled, pitted, and diced",
+        "1 cup (packed) fresh basil leaves",
+        "1/3 cup pine nuts",
+        "3 cloves garlic, chopped",
+        "Juice of 1 lemon",
+        "2 tablespoons extra-virgin olive oil",
+        "Water as needed",
+        "Salt and pepper to taste"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Condiment/Spread",
+        "Side",
+        "No-Cook",
+        "Vegetarian",
+        "Quick & Easy",
+        "Basil",
+        "Pine Nut",
+        "Avocado",
+        "Healthy",
+        "Vegan",
+        "Sugar Conscious",
+        "Pescatarian",
+        "Paleo",
+        "Dairy Free",
+        "Wheat/Gluten-Free",
+        "Peanut Free",
+        "Soy Free",
+        "No Sugar Added",
+        "Kosher"
+    ],
+    "title": "Creamy Avocado Pesto",
+    "url": "http://www.epicurious.com/recipes/food/views/creamy-avocado-pesto-395594"
+}
+
+var r8 = {
+    "directions": [
+        "Put the potatoes in a large pot and cover with cold water. Bring to a boil, over medium heat, and heavily salt the water. Reduce the heat and simmer until tender, about 20 minutes. Steam the cabbage in a steamer sitting over the potato pot. They should be ready at the same time.",
+        "Meanwhile, brown the sausage on all sides in a large saute pan over medium-high heat. Remove the sausage to a plate once it has browned. Add the onions to the pan season with salt and pepper, to taste. Cook the onions until caramelized, adding water, a teaspoon at a time, to deglaze the pan. Stir in the Dijon mustard and 3/4 cup water and return the sausage to the pan to finish cooking. Simmer until the sausage is cooked through and water has reduced to a glaze over the onions, about 7 minutes.",
+        "Drain the potatoes, reserving some of the cooking water. Rice the potatoes into a bowl and add a bit of the potato cooking water to soften. Season with salt and pepper, to taste. Fold in the steamed cabbage and taste for seasonings, adjusting as necessary. Transfer the bangers and mash to a serving dish and dig in!!!!"
+    ],
+    "ingredients": [
+        "6 large potatoes, peeled and cut into large chunks",
+        "Kosher salt",
+        "1/4 savoy cabbage, thinly shredded",
+        "6 large good quality pork sausages",
+        "1 large purple or red onion, thinly sliced",
+        "Freshly ground black pepper",
+        "Water",
+        "2 tablespoons whole-grain Dijon mustard"
+    ],
+    "language": "en-US",
+    "source": "www.foodnetwork.com",
+    "tags": [
+        "Cabbage Recipes",
+        "Vegetable",
+        "Sausage Recipes"
+    ],
+    "title": "Bangers and Mash",
+    "url": "http://www.foodnetwork.com/recipes/claire-robinson/bangers-and-mash-recipe"
+}
+
+var r9 = {
+    "directions": [
+        "Position rack in center of oven and preheat to 375\u00b0F. Butter heavy large baking sheet. Cook bacon in large nonstick skillet over medium-high heat until brown and crisp, about 8 minutes. Transfer bacon to paper towels. Crumble bacon into small pieces. Reserve 2 tablespoons bacon dripping; discard remainder.",
+        "Mix flour, cornmeal, sugar, baking powder and salt in large bowl to blend. Add butter and rub in with fingertips until mixture resembles coarse meal. Whisk 2 eggs, buttermilk, sage and reserved 2 tablespoons bacon drippings in medium bowl to blend. Add to flour mixture and stir until moist dough forms. Mix in corn and bacon.",
+        "Turn dough out onto floured surface and knead gently until smooth, about 8 turns. Roll out dough on work surface to 10x8-inch rectangle (about 3/4 inch thick). Cut rectangle into 12 squares. Place squares on prepared baking sheet, spacing evenly. Brush biscuits with egg glaze. Sprinkle lightly with ground pepper, if desired.",
+        "Bake biscuits until golden and tester inserted into center comes out clean, about 25 minutes. Transfer to rack. Serve warm or at room temperature. (Can be prepared 1 day ahead. Wrap in foil and let stand at room temperature. Rewarm foil-wrapped biscuits in 375\u00b0F oven 5 minutes.)"
+    ],
+    "ingredients": [
+        "8 bacon slices",
+        "2 cups all purpose flour",
+        "1 cup yellow cornmeal",
+        "3 tablespoons sugar",
+        "5 teaspoons baking powder",
+        "1 1/4 teaspoons salt",
+        "1/2 cup plus 2 tablespoons (1 1/4 sticks) chilled unsalted butter, cut into pieces",
+        "2 large eggs",
+        "6 tablespoons buttermilk",
+        "1 3/4 teaspoons (packed) dried rubbed sage",
+        "1 cup thawed drained frozen corn kernels (5 ounces)",
+        "1 egg, beaten to blend (glaze)",
+        "Freshly ground black pepper (optional)"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Bread",
+        "Breakfast",
+        "Brunch",
+        "Bake",
+        "Bacon",
+        "Cornmeal",
+        "Corn"
+    ],
+    "title": "Corn Biscuits with Bacon and Sage",
+    "url": "http://www.epicurious.com/recipes/food/views/corn-biscuits-with-bacon-and-sage-834"
+}
+
+var r10 = {
+    "directions": [
+        "Rinse the cabbage, and then quarter the heads lengthwise. Place the pieces in a large pot or bucket, and sprinkle 1 tablespoon of the salt over them. Set aside for 1 hour.",
+        "Rinse the cabbage under cold running water, and pat it dry. Cut the cabbage into 1-inch pieces, put them in a large bowl, and toss with the remaining 3 tablespoons salt. Let sit for 2 hours. The salt will draw out moisture from the cabbage.",
+        "Drain off the accumulated liquid and lightly rinse the cabbage. In a large bowl, combine the cabbage with all the remaining ingredients, and toss well. Transfer the mixture to a large crock and push the mixture down to force the liquid to rise to the top, somewhat submerging the cabbage in its spicy surroundings. Cover it with a lid and store it in a cool, dark place for 3 days to ferment and mature.",
+        "Once the kimchi is tasting all kinds of yummy, transfer it to clean jars (I like quarts), cap them, and store in the fridge (which will inhibit further fermentation) for up to a month or process according to the jar manufacturer's directions to store on the shelf for up to 9 months."
+    ],
+    "ingredients": [
+        "6 pounds Napa cabbage",
+        "4 tablespoons kosher salt",
+        "2 tablespoons light brown sugar",
+        "1/3 cup minced garlic",
+        "1/3 cup minced fresh ginger",
+        "1/4 cup hot smoked paprika (piment\u00f3n)",
+        "1/4 cup Korean chile powder",
+        "1 tablespoon crushed red pepper flakes",
+        "2 tablespoons freshly squeezed lime juice",
+        "1/2 cup Asian fish sauce",
+        "1/2 cup brine shrimp (available in a jar at Asian food stores), finely chopped",
+        "2 bunches scallions, cut into 1/2-inch pieces (about 12 scallions)"
+    ],
+    "language": "en-US",
+    "source": "www.epicurious.com",
+    "tags": [
+        "Garlic",
+        "Ginger",
+        "Low Fat",
+        "Low Cal",
+        "Healthy",
+        "Cabbage",
+        "Green Onion/Scallion"
+    ],
+    "title": "Classic Cabbage Kimchi",
+    "url": "http://www.epicurious.com/recipes/food/views/classic-cabbage-kimchi-56389571"
+}
+
+
+var output = document.getElementById('output');
+var random = Math.floor(Math.random() * 4)+1; 
+var recipe = r1;
+switch(random) {
+    case 1:
+      // code block
+      recipe = r1;
+      break;
+    case 2:
+      // code block
+      recipe = r2;
+      break;
+    case 3:
+      // code block
+      recipe = r3;
+      break;
+    case 4:
+    // code block
+    recipe = r4;
+    break;
+    case 5:
+    // code block
+    recipe = r5;
+    break;
+    case 5:
+    // code block
+    recipe = r6;
+    break; 
+    case 5:
+    // code block
+    recipe = r7;
+    break; 
+    case 5:
+    // code block
+    recipe = r8;
+    break; 
+    case 5:
+    // code block
+    recipe = r9;
+    break; 
+    case 5:
+    // code block
+    recipe = r10;
+    break;   
+    default:
+      // code block
+      recipe = r1;
+
+  }
+outputTitle.innerHTML = recipe.title;
+for(var i = 0; i < recipe.ingredients.length; i++){
+    var table = document.getElementById("outputIngredients");
+    var row = table.insertRow(1);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = recipe.ingredients[i];
+    
+}
+
+for(var i = 0; i < recipe.directions.length; i++){
+    var table = document.getElementById("outputInstructions");
+    var row = table.insertRow(i+1);
+    var cell1 = row.insertCell(0);
+    cell1.innerHTML = recipe.directions[i];
+    
+}
